5ed054804ced6ac05a222ffe8710fc6354991fbf
Montgomery Teofilus <montgomery2515@gmail.com>
2024-04-20 17:06:28 +0700

    first

---
+++ src/Pages/Home.js
@@ -0,0 +1,146 @@
+import React, { useRef, useEffect } from "react";
+import gsap from "gsap";
+import { personalDetails } from "../Details";
+
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 4;
+  this.period = parseInt(period, 10) || 0;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 100;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+function Home() {
+  const { name, img } = personalDetails;
+  const h11 = useRef();
+  const h12 = useRef();
+  const h13 = useRef();
+  const myimageref = useRef();
+
+  useEffect(() => {
+    const tl = gsap.timeline();
+
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+
+    tl.from(
+      h11.current,
+      {
+        x: "-100%",
+        delay: 0.8,
+        opacity: 0,
+        duration: 2,
+        ease: "Power3.easeOut",
+      },
+      "<"
+    )
+      .from(
+        h12.current,
+        {
+          x: "-100%",
+          delay: 0.5,
+          opacity: 0,
+          duration: 2,
+          ease: "Power3.easeOut",
+        },
+        "<"
+      )
+      .from(
+        h13.current,
+        {
+          x: "-100%",
+          delay: 0.1,
+          opacity: 0,
+          duration: 2,
+          ease: "Power3.easeOut",
+        },
+        "<"
+      )
+      .from(
+        myimageref.current,
+        {
+          x: "200%",
+          delay: 0.5,
+          opacity: 0,
+          duration: 2,
+          ease: "Power3.easeOut",
+        },
+        "<"
+      );
+  }, []);
+
+
+
+
+  return (
+    <main className="container mx-auto max-width section md:flex justify-between items-center">
+      <div>
+        <h1
+          ref={h11}
+          className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold"
+        >
+          Hi,👋<br></br>My Name is<br></br>
+        </h1>
+        <h1
+          ref={h12}
+          className="text-2xl bg-clip-text bg-gradient text-transparent md:text-4xl xl:text-5xl xl:leading-tight font-bold"
+        >
+          {name}
+        </h1>
+        <h2
+          ref={h13}
+          className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold"
+        >
+          I'm a&nbsp;
+          <span class="txt-rotate color" data-period="1200" data-rotate=
+          '["Coder", "Web Developer", "Mobile App Developer"]'></span>
+        </h2>
+      </div>
+      <div className="mt-5 md:mt-0">
+        <img ref={myimageref} className="w-1/2 md:ml-auto" src={img} alt="Montgomery" />
+      </div>
+    </main>
+
+  );
+}
+
+export default Home;
